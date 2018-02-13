0add7bf3d108e569094699614202a12cf4e52bad
Raquel <rsmendoza1@gmail.com>
2018-02-12 18:54:41 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,216 @@
+/*elint-env browser*/
+
+//STEP 1
+/* 
+var someMonth 
+function getCurrentMonth // function to return current month 
+current_month // a constant 
+var summerMonth // an array of summer months 
+myLibraryFunction // a function 
+*/
+
+//STEP 2
+/* 
+11.3 // numeric literal 
+"Pura Vida!" // string literal 
+true // Boolean literal 
+null // literial null value 
+*/ 
+
+//STEP 3
+/* 
+var anExpression = 2 * (16 / 4) + 3; 
+var aSecondExpression = Math.PI * radius * radius 
+var aThirdExpression = aSecondExpression + "%" + anExpression
+var aFourthExpression = "(" + aThirdExpression + ") + (" + aSecondExpression")";
+*/
+
+//STEP 4
+/*
+var strFirstName; 
+var strLastName;
+var address; 
+var city; 
+var state; 
+var zipcode; /null for numbers? 
+var yourAge; 
+var referralSource; 
+var mayWeContactYou
+*/
+
+//STEP 5
+/*
+var strFirstName;
+var strLastName; 
+strFirstName = "Raquel";
+strLastName = "Mendoza";
+strFirstName = true;
+strLastName = true; 
+
+var address = "123 FEWD";
+address = "true"; 
+
+var city = "San Diego", city = true; 
+
+var state;
+var zipcode; 
+state = "CA";
+zipcode = "91950";
+state = true; 
+zipcode = true; 
+
+var yourAge = "32"; 
+yourAge = "true"; 
+
+var referralSource; 
+referralSource = "friend"; 
+referralSource = true; 
+
+var mayWeContactYou = "Yes", mayWeContactYou = true; 
+*/
+
+//STEP 6 
+/*
+var wakeUp = "5";
+window.console.log ("Raquel wakes up at " + wakeUp + " in the morning."); 
+*/ 
+
+//STEP 7
+/*
+car = "Jeep";
+var car; 
+window.console.log(car);
+*/
+
+//STEP 8
+/*
+var someString = "Who once said, 'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.'";
+window.console.log(someString);
+*/
+
+//STEP 9 
+/*
+var x = 100; 
+var y = null; 
+x = null; 
+window.console.log(x); 
+window.console.log(y);
+
+var x; 
+window.console.log(x);
+*/
+
+//STEP 10
+/*
+typeof "Lima"
+typeof "11.3"
+typeof "false"
+typeof {name:'Lima', age:32}
+typeof "myCar"
+*/
+
+//STEP 11
+/*
+window.alert("Hello Raquel Mendoza, welcome to the JavaScript class!"); 
+
+var x = "Hello Raquel Mendoza";
+var y = "welcome to the JavaScript class!";
+window.console.log (x + " " + y);
+*/
+
+//STEP 12
+/*
+var firstName = "Raquel"
+*/
+
+//STEP 13
+/*
+var course = "JavaScript"; 
+*/
+
+//STEP 14
+/*
+var stringValue = "Hello Raquel Mendoza \nwelcome to the JavaScript class!";
+window.console.log(stringValue);
+*/
+
+//STEP 15
+/*
+var person = prompt ("Please enter your name", "Raquel Mendoza"); 
+
+window.console.prompt()
+*/
+
+//STEP 16
+/*
+var x = 10;
+var y = 20; 
+window.console.log(x + y);
+*/
+
+//STEP 17
+/*
+var x = 20;
+var y = 20; 
+window.console.log(x + y);
+*/
+
+//STEP 18
+/*
+var x = 20;
+var x = 20;
+*/
+
+//STEP 19
+/*
+var x = 20; 
+x * = 5;
+*/
+
+//STEP 20 
+/*
+var x = 20; 
+var y = 3; 
+window.console.log (x == y);
+*/
+
+//STEP 21
+/*
+null  == undefined; 
+var x = 5; 
+var y = 10; 
+window.console.log (x <= y != true);
+*/
+
+//STEP 22
+/*
+null === undefined; 
+var x = 5; 
+var y = 10;
+window.console.log (x => y !=false); 
+*/
+
+//STEP 23
+
+
+//STEP 24
+/*
+string s = "Garfield is a cat"
+return s instanceof String; 
+// true
+*/
+
+//STEP 25
+/*
+string sentence = null;
+string sentence instanceof String; 
+//false 
+*/
+
+
+
+
+
+
+
+
